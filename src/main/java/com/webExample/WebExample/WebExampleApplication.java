c4a751533a40aae7d4cf7a81651a645c6cf54a88
cristiano-hahn <cristianohahn.ch@gmail.com>
2018-05-15 20:28:41 -0300

    Hello world

---
+++ src/main/java/com/webExample/WebExample/WebExampleApplication.java
@@ -0,0 +1,12 @@
+package com.webExample.WebExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebExampleApplication.class, args);
+	}
+}
